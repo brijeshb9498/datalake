b0bafcb1b1836e108647bb38bc7e62b907e495b3
Brijesh <134255343+brijeshb9498@users.noreply.github.com>
2024-06-20 19:19:31 +0530

    Update s3.tf

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -4,13 +4,12 @@ resource "aws_s3_bucket_lifecycle_configuration" "data_lake" {
   rule {
     
     id      = "move-to-glacier"
-    enabled = true
-
+    
     transition {
       days          = 30
       storage_class = "STANDERD_IA"
     }
-    trasition {
+    transition {
       days          = 365
       storage_class = "GLACIER"
   }
