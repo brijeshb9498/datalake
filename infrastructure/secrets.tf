930cdd82db83e4d0498b516d055c28b2be0b8703
Brijesh <134255343+brijeshb9498@users.noreply.github.com>
2024-06-20 21:08:53 +0530

    Update secrets.tf

--- infrastructure/secrets.tf
+++ infrastructure/secrets.tf
@@ -1,9 +1,9 @@
-resource "aws_secretsmanager_secret" "sftp_secret" {
+resource "aws_secretsmanager_secret" "sftp1_secret" {
   name = "sftp/agency_user_password"
 }
 
-resource "aws_secretsmanager_secret_version" "sftp_secret_value" {
-  secret_id     = aws_secretsmanager_secret.sftp_secret.id
+resource "aws_secretsmanager_secret_version" "sftp1_secret_value" {
+  secret_id     = aws_secretsmanager_secret.sftp1_secret.id
   secret_string = jsonencode({
     password = "password35"
   })
