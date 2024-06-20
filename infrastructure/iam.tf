ad67d16032de0864ede7c7d35f0eea992f1afdc8
Brijesh <134255343+brijeshb9498@users.noreply.github.com>
2024-06-20 18:51:28 +0530

    Update iam.tf

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -41,3 +41,27 @@ resource "aws_iam_role_policy_attachment" "sftp_role_attach" {
   role       = aws_iam_role.sftp_role.name
   policy_arn = aws_iam_policy.sftp_policy.arn
 }
+resource "aws_transfer_user" "sftp_user" {
+  user_name   = "agency_user"
+  server_id   = aws_transfer_server.sftp.id
+  role        = aws_iam_role.sftp_role.arn
+  home_directory = "/my-data-lake-bucket/agency_user"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "s3:ListBucket"
+        Resource = "arn:aws:s3:::my-data-lake-bucket"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "arn:aws:s3:::my-data-lake-bucket/agency_user/*"
+      }
+    ]
+  })
+}
