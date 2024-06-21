283ad2febfb2768dd47de9b11ba67fc1eec7a3a4
Brijesh <134255343+brijeshb9498@users.noreply.github.com>
2024-06-21 08:13:11 +0530

    Update sftp.tf

--- infrastructure/sftp.tf
+++ infrastructure/sftp.tf
@@ -1,7 +1,13 @@
-resource "aws_transfer_server" "sftp" {
-  endpoint_type = "PUBLIC"
-  identity_provider_type = "SERVICE_MANAGED"
+resource "aws_transfer_server" "example" {
+  endpoint_type = "VPC"
+
+  endpoint_details {
+    address_allocation_ids = [aws_eip.id]
+    subnet_ids             = [aws_subnet.id]
+    vpc_id                 = aws_vpc.id
+    identity_provider_type = "SERVICE_MANAGED"
   tags = {
     Name = "SFTPServer"
   }
+}
 }
